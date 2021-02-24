9d0045475986395ca8153854481d2c453aedc907
Vladimir Godiiak <vladimir_godiiak@mail.ru>
2021-02-24 18:54:18 +0300

    Generate empty project

---
+++ src/main/java/com/example/dictionar/DictionarApplication.java
@@ -0,0 +1,13 @@
+package com.example.dictionar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictionarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dictionar/DictionarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dictionar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
